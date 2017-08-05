55b803a0a17a631b508fe60473a0f27ab18e2237
thor <send.tilmig@gmail.com>
2017-08-05 21:20:56 +0200

    I gang med week 4

--- week3.R
+++ week3.R
@@ -2,17 +2,47 @@
 #           E2.9
 ##################################
 
-
+## Ikke relevante bare små tests 
+qnorm(pnorm(2))
+pnorm(-2)
 
 ##################################
 #           E2.10
 ##################################
+# Let X be normally distributed with mean 24 and variance 16
+# Calculate the following probabilities:
 
+xMean <- 24
+xVariance <- 16
+xStdDiv <- sqrt(xVariance)
 
+#   P(X ≤ 20)
+x <- 20
+pnorm(x, mean = xMean, sd = xStdDiv)
+# [1] 0.1586553
+
+#   P(X > 29.5)
+x <- 29.5
+1 - pnorm(x, mean = xMean, sd = xStdDiv)
+# [1] 0.08456572
+
+#   P(X = 23.8)
+x <- 23.8
+dnorm(x, mean = xMean, sd = xStdDiv)
+# [1] 0.09961098
 
 ##################################
 #           E2.11
 ##################################
+
+##  a)
+# Y ~ N(11, 25) og P(Y < 10)
+xMean <- 11
+xVariance <- 25
+xStdDiv <- sqrt(xVariance)
+x <- 10
+
+pnorm(x, mean = xMean, sd = xStdDiv)
 
 
 
@@ -20,19 +50,35 @@
 #           E2.12
 ##################################
 
+##  a)
+xMean <- 3000
+xVariance <- 9
+xStdDiv <- sqrt(xVariance)
+x1 <- 2993 
+x2 <- 3007
+
+pnorm(x1, mean = xMean, sd = xStdDiv) + 1 -pnorm(x2, mean = xMean, sd = xStdDiv)
+
+##  b)
+
 
 
 ##################################
 #           E2.13
 ##################################
 
+##  a)
+lambdaYear <- 110000
+lambdaDay  <- lambdaYear/365
+lambdaHour <- lambdaDay/24
+lambdaMin  <- lambdaHour/60
+min <- 30
 
+dpois(x=0, lambda=lambdaMin*30)
 
+##  b)
 
-
-
-
-
+1-pexp(1, lambdaMin*15)
 
 
 
---
+++ week4.R
@@ -0,0 +1,49 @@
+stdErrorOfMean <- function(dataSet){
+  stdMerror <- (sd(dataSet))/sqrt(length(dataSet))
+  cat("std-error of mean is -> ")
+  return(stdMerror)
+}
+
+
+
+
+##################################
+#           E3.1
+##################################
+ci <- c(3003, 3005, 2997, 3006, 2999, 2998, 3007, 3005, 3001)
+µ <- 3000
+
+
+##  a)
+mean(ci)
+sd(ci)
+
+stdErrorOfMean(ci)
+
+##  b)
+xMean <- 3000
+xStdDiv <- 3 
+
+
+
+
+##  c)
+
+
+##  d)
+
+
+##  e)
+
+
+##  f)
+
+
+
+
+
+
+
+##################################
+#           E3.2
+##################################
