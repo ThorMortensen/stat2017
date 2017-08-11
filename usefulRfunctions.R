6eb549f690f3524ada517eb6fd973bfa123d0f2e
thor <send.tilmig@gmail.com>
2017-08-11 18:54:14 +0200

    Finished week6

--- usefulRfunctions.R
+++ usefulRfunctions.R
@@ -22,7 +22,7 @@ confIntervalVariance <- function(varince, df, conf.level){
 }
 
 confIntervalData_Variance_StdDiv <- function(dataSet, conf.level){
-  varince <- sd(ci)^2
+  varince <- sd(dataSet)^2
   df <- length(dataSet)-1
   confResult <- confIntervalVariance(varince = varince, df = df, conf.level = conf.level)
   cat("Confidence interval for ")
@@ -36,7 +36,7 @@ confIntervalMean <- function(n, mean, stdDiv, conf.level){
     mean - qt((1-conf.level)/2, df = n-1) * stdDiv / sqrt(n))
 }
 
-scientificNotation <- function(isOn){
+setScientificNotation <- function(isOn){
   if(isOn == TRUE){
     options(scipen=0)
   }else {
@@ -88,25 +88,58 @@ oneSampleTTest <- function(h0, sampleMean, sdtDiv, n){
   cat("\nt-obs    --> ", t_obs)
 }
 
-twoSampleT_test <- function(ns, means, stdDivs){
+twoSampleT_test <- function(ns, means, stdDivs, h0 = 0){
   if(length(ns) < 2){
     ns <- c(ns,ns)
   }
   
-  vs <- stdDivs^2
+  variance <- stdDivs^2
   
-  t_obs <- abs((means[2]-means[1])/sqrt(vs[1]/ns[1]+vs[2]/ns[2]))
+  t_obs <- ((abs(means[2]-means[1])-h0)/sqrt(variance[1]/ns[1]+variance[2]/ns[2]))
   ## The degrees of freedom
-  v <- ((vs[1]/ns[1]+vs[2]/ns[2])^2)/
-    ((vs[1]/ns[1])^2/(ns[1]-1)+(vs[2]/ns[2])^2/(ns[2]-1))
+  degreeFreedom_v <- ((variance[1]/ns[1]+variance[2]/ns[2])^2)/
+    ((variance[1]/ns[1])^2/(ns[1]-1)+(variance[2]/ns[2])^2/(ns[2]-1))
   
-  pValue <- 2*(1 - pt(t_obs, df = v))
+  pValue <- 2*(1 - pt(t_obs, df = degreeFreedom_v))
   
-  cat("t-obs    -->", t_obs)
-  cat("\ndf       -->", v)
+  cat("\n\ntwoSampleT_test")
+  cat("\nt-obs    -->", t_obs)
+  cat("\ndf       -->", degreeFreedom_v)
   cat("\np-value  -->",pValue)
   
 }
+
+
+
+twoSampleConfInterval <- function( ns, means, stdDivs, conf.level = 0.95){
+  if(length(ns) < 2){
+    ns <- c(ns,ns)
+  }
+  variance <- stdDivs^2
+  
+  meanDif <- ((means[1] - means[2]))
+  
+  degreeFreedom_v <- ((variance[1]/ns[1]+variance[2]/ns[2])^2)/
+    ((variance[1]/ns[1])^2/(ns[1]-1)+(variance[2]/ns[2])^2/(ns[2]-1))
+  
+  t_1MinusAlphaDiv2 <-  (qt((1-conf.level)/2, df = degreeFreedom_v))
+  
+  calc <- sqrt((variance[1] / ns[1]) + (variance[2] / ns[2]))
+  
+  confInterval <- c(meanDif + t_1MinusAlphaDiv2 * calc,
+                    meanDif - t_1MinusAlphaDiv2 * calc)
+  
+  cat("variance                       -->" ,variance)
+  cat("\ndegreeFreedom_v                -->",degreeFreedom_v)
+  cat("\nt-dist ( 1 − α/2 )-quantile    -->", abs(t_1MinusAlphaDiv2))
+  cat("\nconfidence interval            -->", confInterval)
+}
+
+# twoSampleT_testAll( ns, means, stdDivs, conf.level = 0.95, h0 = 0){
+#   twoSampleConfInterval(ns = ns,means = means,stdDivs = stdDivs, conf.level = conf.level)
+#   twoSampleT_test(ns = ns,means = means,stdDivs = stdDivs, h0 = h0)
+# }
+
 
 ##################################
 #           E
--- week6.R
+++ week6.R
@@ -7,22 +7,35 @@ source("usefulRfunctions.R")
 meanA   <- 1.93
 meanB   <- 1.49
 
+means <- c(meanA, meanB)
+
 stdDivA <- 0.45
 stdDivB <- 0.58
+
+stdDivs <- c(stdDivA, stdDivB)
 
 n <- 9
 
 
 ##  a)
 setNumberOfDigits(5)
-twoSampleT_test(9, c(meanA, meanB), c(stdDivA, stdDivB))
-
-
+twoSampleT_test(9, means, stdDivs)
 
 ##  b)
+setNumberOfDigits(3)
+twoSampleConfInterval(ns = 9,
+                      means = means,
+                      stdDivs = stdDivs)
+
 ##  c)
+power.t.test(n = 9, delta=0.4, sd=0.5, sig.level = 0.05)
+
 ##  d)
+power.t.test(n = 9, power = 0.8, sd=0.5, sig.level = 0.05)
+
 ##  e)
+power.t.test(delta = 0.4, power = 0.9, sd=0.5, sig.level = 0.05)
+
 ##  f)
 ##  g)
 
@@ -31,12 +44,13 @@ twoSampleT_test(9, c(meanA, meanB), c(stdDivA, stdDivB))
 ##################################
 
 ##  a)
-##  b)
-##  c)
-##  d)
-##  e)
-##  f)
-##  g)
+x1 <- c(9.1, 8.0, 7.7, 10.0, 9.6, 7.9, 9.0, 7.1, 8.3,
+        9.6, 8.2, 9.2, 7.3, 8.5, 9.5)
+x2 <- c(8.2, 6.4, 6.6, 8.5, 8.0, 5.8, 7.8, 7.2, 6.7,
+        9.8, 7.1, 7.7, 6.0, 6.6, 8.4)
+setNumberOfDigits(4)
+setScientificNotation(FALSE)
+t.test(x1, x2, conf.level = 1-0.001,pair=TRUE)
 
 
 
@@ -44,35 +58,83 @@ twoSampleT_test(9, c(meanA, meanB), c(stdDivA, stdDivB))
 #           E3.7
 ##################################
 
-##  a)
-##  b)
-##  c)
-##  d)
-##  e)
-##  f)
-##  g)
+Pulse_end <-  c(173,175,174,183,181,180,170,182,188,178,181,183,185)
+Pulse_1min <- c(120,115,122,123,125,140,108,133,134,121,130,126,128)
 
+
+mean(Pulse_end)
+mean(Pulse_1min)
+sd(Pulse_end)
+sd(Pulse_1min)
+sd(Pulse_end-Pulse_1min)
+
+##  a)
+
+t.test(x = Pulse_end,
+       y = Pulse_1min,
+       paired = TRUE,
+       conf.level = 0.99, 
+       )
+
+##  b)
+confIntervalData_Variance_StdDiv(dataSet = Pulse_end,
+                                 conf.level = 0.95)
 
 
 ##################################
 #           E3.8
 ##################################
 
+meanMax <- 2.508
+meanMin <- 2.103
+
+sdMax <- 0.3373
+sdMin <- 0.2834
+
+
+  
+
 ##  a)
+confIntervalMean(n = 10,
+                 mean = meanMax - meanMin,
+                 stdDiv = 0.09664,
+                 conf.level = 0.95)
+
 ##  b)
-##  c)
-##  d)
-##  e)
-##  f)
-##  g)
+oneSampleTTest(h0 = 0.35,
+               sampleMean = meanMax - meanMin,
+               sdtDiv = 0.09664,
+               n = 10)
 
 ##################################
 #           E3.9
 ##################################
+beforeMean <- 6.420
+afterMean <- 7.375
+
+beforeSd <- 2.205
+afterSd <- 1.813
+
+beforeN <- 50
+afterN <- 24
+
 
 ##  a)
+twoSampleT_test(ns = c(beforeN,afterN),
+                means = c(beforeMean,afterMean),
+                stdDivs = c(beforeSd, afterSd))
+
 ##  b)
+setNumberOfDigits(7)
+twoSampleConfInterval(ns = c(beforeN,afterN),
+                      means = c(beforeMean,afterMean),
+                      stdDivs =  c(beforeSd, afterSd),
+                      conf.level = 0.99)
 ##  c)
+sqrt(confIntervalVariance(df = afterN-1,
+                     varince = afterSd^2,
+                     conf.level = 0.95))
+
 ##  d)
 ##  e)
 ##  f)
